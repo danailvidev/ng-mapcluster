09d5ce5f5398a2cedee650bdee7519d2170508b0
Danail Videv <danailvidev@gmail.com>
2019-09-09 13:11:59 +0300

    add pwa lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'npo-root',
-  template: `<router-outlet></router-outlet>`
+  template: `
+  <button *ngIf="newVersion" nz-button [nzType]="'primary'" (click)="reload()">Нова версия</button>
+  <router-outlet></router-outlet>
+  `
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  newVersion: any = false;
+
+  constructor(public swUpdate: SwUpdate, private message: NzMessageService) { }
+
+  ngOnInit() {
+    this.swUpdate.available
+      .subscribe(update => this.newVersion = true);
+  }
+
+  reload() {
+    this.swUpdate.activated
+      .subscribe(update => window.location.reload());
+    this.swUpdate.activateUpdate();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
 
+import { NzMessageModule } from 'ng-zorro-antd/message';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
+
+import { environment } from '../environments/environment';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBGfbFGm-4kKEk-rfjdmY8qMMZqZT25HWE',
@@ -31,7 +37,10 @@ const firebaseConfig = {
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFireAuthModule,
     HttpClientModule,
-    HttpClientJsonpModule
+    HttpClientJsonpModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    NzMessageModule,
+    NzButtonModule
   ],
   providers: [AngularFirestore],
   bootstrap: [AppComponent]
