aa3ad250159c19d35f7f2a66b0511e34f824fea7
Danail Videv <danailvidev@gmail.com>
2019-09-10 14:36:18 +0300

    add missing imports

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -5,10 +5,13 @@ import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzDrawerModule } from 'ng-zorro-antd/drawer';
+import { NzMessageModule } from 'ng-zorro-antd/message';
 
 import { LayoutComponents, LayoutRouting } from './layout.routing';
 import { NavComponent } from './nav/nav.component';
 import { SettingsComponent } from './settings/settings.component';
+import { SettingsFormModule } from './settings/settings-form/settings-form.module';
+import { SettingsService } from './settings/settings.service';
 
 @NgModule({
   declarations: [LayoutComponents.components, NavComponent, SettingsComponent],
@@ -18,9 +21,12 @@ import { SettingsComponent } from './settings/settings.component';
     CommonModule,
     LayoutRouting,
     NzIconModule,
-    NzDrawerModule
+    NzDrawerModule,
+    NzMessageModule,
+    SettingsFormModule
   ],
   providers: [
+    SettingsService
   ]
 })
 export class LayoutModule { }
